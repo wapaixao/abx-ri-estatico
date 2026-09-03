db2a8f274f2a329b3c32280e9a6ee47ca7d267a4
Platao Hermes <platao-hermes@users.noreply.github.com>
2026-09-03 21:24:13 +0000

    Usa matriz U006 corrigida no build

--- build_abx_ri.py
+++ build_abx_ri.py
@@ -8,7 +8,7 @@ from abx_u006_rules import apply_u006_structural_rules, append_u006_month_from_c
 ROOT = Path('/root/content/sites/abx-ri-estatico')
 DATA_PATH = ROOT/'data.json'
 DRU_XLSX = Path('/root/data/abx/entregas/APRESENTACAO/ABX_DRU_RI_Receita_Gerencial_U006_VALIDACAO_V5_CF_SPLIT.xlsx')
-U006_XLSX = Path('/root/data/abx/entregas/APRESENTACAO/ABX_Receita_Gerencial_U006_1T_2T2026_VALIDACAO.xlsx')
+U006_XLSX = Path('/root/data/abx/entregas/APRESENTACAO/ABX_Receita_Gerencial_U006_1T_2T2026_VALIDACAO_MATRIZ_CORRIGIDA.xlsx')
 PISCOFINS_XLSX = Path('/root/data/abx/APURACAO_COFINS_PIS_2T2026_COMPLETA_FORMATADA_SEM_OBS.xlsx')
 RESUMO_XLSX = Path('/root/data/abx/APURACAO_COFINS_PIS_2T2026_COMPLETA_FORMATADA_SEM_OBS_COM_RESUMO_VALIDACAO_V4.xlsx')
 MONTHLY_U006_SOURCES = [
